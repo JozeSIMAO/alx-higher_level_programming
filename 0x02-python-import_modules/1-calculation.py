ddd983a0a7de7d0320f1f546be8eca9cdc4d7d1f
JozeSIMAO <jozesimao10@gmail.com>
2023-08-10 07:53:11 -0700

    1-calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+# A program that imports functions from the calculator_1.py file
+# and does some Maths and then prints the results
+
+a = 10
+b = 5
+
+print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} - {b} = {sub(a, b)}")
+print(f"{a} * {b} = {mul(a, b)}")
+print(f"{a} / {b} = {div(a, b)}")

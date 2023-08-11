5b0a958448e2af807e95ce7c399b2178b1deea2a
JozeSIMAO <jozesimao10@gmail.com>
2023-08-11 05:13:57 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-import sys
-# A program that prints the number of
-# and the lists of its arguments
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv)
+    counter = 1
 
-num_args = len(sys.argv)
-counter = 1
-
-if num_args == 1:
-    print("0 arguments.")
-elif num_args > 1:
-    print(f"{num_args - 1} arguments:")
-    while counter < num_args:
-        print(f"{counter}: {sys.argv[counter]}")
-        counter += 1
+    if num_args == 1:
+        print("0 arguments.")
+    elif num_args > 1:
+        print(f"{num_args - 1} arguments:")
+        while counter < num_args:
+            print(f"{counter}: {sys.argv[counter]}")
+            counter += 1

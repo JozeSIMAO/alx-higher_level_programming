ff2bd29fb900cf48b86fb6f637da54c1882671d0
JozeSIMAO <jozesimao10@gmail.com>
2023-08-11 05:31:32 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_args = len(sys.argv)
-    counter = 1
+    num_args = len(sys.argv) - 1
 
-    if num_args == 1:
+    if num_args == 0:
         print("0 arguments.")
-    elif num_args > 1:
-        print(f"{num_args - 1} arguments:")
-        while counter < num_args:
-            print(f"{counter}: {sys.argv[counter]}")
-            counter += 1
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+    for i in range(num_args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

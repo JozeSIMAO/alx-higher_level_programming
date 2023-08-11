35b1914b4adbd4447fb6b7a914fbe6ddc2ec0d23
JozeSIMAO <jozesimao10@gmail.com>
2023-08-11 05:33:53 -0700

    3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-import sys
-# A program that prints the result of the addition of all arguments
-# Followed by a new line
+if __name__ = "__main__":
+    import sys
+    counter = 1
+    num_args = len(sys.argv)
+    sum = 0
 
-counter = 1
-num_args = len(sys.argv)
-sum = 0
-
-while counter < num_args:
-    sum = sum + int(sys.argv[counter])
-    counter += 1
-print(f"{sum}")
+    while counter < num_args:
+        sum = sum + int(sys.argv[counter])
+        counter += 1
+    print(f"{sum}")

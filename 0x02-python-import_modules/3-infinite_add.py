43287de0357f99d5b7aa95ac09b39ff04e24fe96
JozeSIMAO <jozesimao10@gmail.com>
2023-08-11 05:40:32 -0700

    3-infinte_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     import sys
-    counter = 1
     num_args = len(sys.argv)
     sum = 0
+    counter = 0
 
-    while counter < num_args:
-        sum = sum + int(sys.argv[counter])
-        counter += 1
+    while counter < (num_args - 1):
+        sum = sum + int(sys.argv[counter + 1])
     print("{}".format(total))

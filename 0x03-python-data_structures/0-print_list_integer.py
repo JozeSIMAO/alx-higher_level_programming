2990fdedbc9c965e8802d5dc9de5d010421736c8
JozeSIMAO <jozesimao10@gmail.com>
2023-08-12 04:52:14 -0700

    print all integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """A function that prints all integers of a string"""
     for integer in my_list:
-        if isinstance(integer,int):
-            print("{}".format(integer))
+        print("{:d}".format(integer))

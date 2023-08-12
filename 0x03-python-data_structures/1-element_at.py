80d0b14346605a3eafccd7f0ac1042db129b8912
JozeSIMAO <jozesimao10@gmail.com>
2023-08-12 05:09:19 -0700

    retrieve element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """A function that retrieves an element from a list"""
+    if idx < 0:
+        return None
+    if idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])

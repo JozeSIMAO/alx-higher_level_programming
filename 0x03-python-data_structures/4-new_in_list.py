5ede0afe02349bee0f1b30ad5112cdaf6cd909cc
JozeSIMAO <jozesimao10@gmail.com>
2023-08-13 14:23:48 -0700

    replace an element

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """a function that replaces an element in a list at a specific
+    position without modifying the original list"""
+    new_list = my_list[:]
+    i = 0
+    if idx < 0 or idx > (len(my_list)):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list

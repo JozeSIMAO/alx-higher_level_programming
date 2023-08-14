303ebd24dfb405c286e8395fc5279b7a184e65ba
JozeSIMAO <jozesimao10@gmail.com>
2023-08-14 13:22:46 -0700

    addtuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """a function that adds 22 tuples"""
-    pad_tuple_a = tuple_a + (0, 0)
-    pad_tuple_b = tuple_b + (0, 0)
+    p_tuple_a = tuple_a + (0, 0)
+    p_tuple_b = tuple_b + (0, 0)
 
     new_tuple = (p_tuple_a[0] + p_tuple_b[0], p_tuple_a[1] + p_tuple_b[1])
     return new_tuple

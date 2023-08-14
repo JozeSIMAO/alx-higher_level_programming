db67ff020748e6fe9e2d8a406f0c5489c428ade7
JozeSIMAO <jozesimao10@gmail.com>
2023-08-14 03:20:08 -0700

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     padded_tuple_a = tuple_a + (0, 0)
     padded_tuple_b = tuple_b + (0, 0)
 
-    new_tuple = (padded_tuple_a[0] + padded_tuple_b[0], padded_tuple_a[1]
-            + padded_tuple_b[1])
+    new_tuple = (padded_tuple_a[0] + padded_tuple_b[0], padded_tuple_a[1] + padded_tuple_b[1])
     return new_tuple

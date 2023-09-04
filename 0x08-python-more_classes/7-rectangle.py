9a4e553bc542ad021a2def1dc7bf761ba7d9d01b
JozeSIMAO <jozesimao10@gmail.com>
2023-09-04 14:00:01 -0700

    change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,18 +1,8 @@
-#!/usr/bin/python3
-"""defines a class Rectangle"""
-
-
 class Rectangle:
-    """represents a rectangle"""
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initializes a new rectangle
-        Args:
-            width (int): width of the rectangle
-            height (int): height of the rectangle
-        """
         self.__width = 0
         self.__height = 0
         self.width = width
@@ -44,25 +34,23 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
-            return (0)
-        return (2 * (self.__width + self.__height))
+            return 0
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            return ("")
+            return ""
         rectangle = str(self.print_symbol) * self.__width
         for i in range(self.__height - 1):
-            rectangle += str(self.print_symbol) * self.__width
-            if i < self.__height - 1:
-                rectangle += "\n" + str(self.print_symbol) * self.__width
-        return (rectangle)
+            rectangle += "\n" + str(self.print_symbol) * self.__width
+        return rectangle
 
     def __repr__(self):
-        return ("Rectangle({}, {})".format(self.__width, self.__height))
+        return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
         Rectangle.number_of_instances -= 1

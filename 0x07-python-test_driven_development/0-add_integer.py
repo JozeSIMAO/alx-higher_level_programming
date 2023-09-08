ad4f991085f27f84b0636bc65a13e4e171f1f338
JozeSIMAO <jozesimao10@gmail.com>
2023-09-08 13:42:55 -0700

    adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 # 0-add_integer.py
-"""Defines an integer addition function."""
+"""defines a function that adds 2 integers"""
 
 
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
 
-    Float arguments are typecasted to ints before addition is performed.
+    Args:
+        a: 1st integer
+        b: 2nd integer
 
     Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+        TypeError: If either of a or b is a non-integer and not a flot
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
+
     return (int(a) + int(b))

2417d1a8eb54c1cb576ac334fb4d78753c0c082a
JozeSIMAO <jozesimao10@gmail.com>
2023-09-08 13:57:49 -0700

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,7 @@
 #!/usr/bin/python3
 # text_indentation.py
-"""defines a text indentation function
+"""defines a text indentation function"""
 
-This module provides a function, text_indentation, which takes a text string as input and
-prints the text with two new lines after each '.', '?', and ':' characters.
-
-Example:
-    text = "Hello. This is a sample text: It has multiple sentences. Is it working?"
-    text_indentation(text)
-    
-    Output:
-    Hello.
-    
-    This is a sample text:
-    
-    It has multiple sentences.
-    
-    Is it working?
-
-Args:
-    text (str): The input text to be formatted.
-
-Raises:
-    TypeError: If text is not a string.
-"""
 
 def text_indentation(text):
     """Print text with two new lines after each '.', '?', and ':'.
@@ -50,4 +28,3 @@ def text_indentation(text):
                 char += 1
             continue
         char += 1
-

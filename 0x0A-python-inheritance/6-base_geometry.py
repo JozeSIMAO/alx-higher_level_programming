14dc7225481bb67a5f9af31f659631c9af242127
JozeSIMAO <jozesimao10@gmail.com>
2023-09-11 09:03:57 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeomeytry"""
+
+
+class BaseGeometry:
+    """represents a class BaseGeometry"""
+
+    def area(self):
+        """not impimented instance"""
+        raise Exception("area() is not implimented")

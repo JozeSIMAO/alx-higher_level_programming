8930d22d08c6f77de957f4ee3e3812145765a0fb
JozeSIMAO <jozesimao10@gmail.com>
2023-11-13 16:47:04 +0200

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined || process.argv[3] == undefined) {
-  console.log('undefined is undefined');
+if (process.argv[3] === undefined) {
+  console.log(`${process.argv[2]} is undefined`);
 } else {
   console.log(`${process.argv[2]} is ${process.argv[3]}`);
 }

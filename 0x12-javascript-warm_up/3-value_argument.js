9e2de17d48a27b24071007b4bd744c156fbba7c8
JozeSIMAO <jozesimao10@gmail.com>
2023-11-13 16:35:20 +0200

    value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,11 +2,8 @@
 
 const argsArray = process.argv;
 
-if (argsArray[2] === undefined)
-{
-    console.log('No argument')
-}
-else
-{
-    console.log(argsArray[2]);
+if (argsArray[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argsArray[2]);
 }

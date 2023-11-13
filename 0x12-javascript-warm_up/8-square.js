41f0af92104c1314764303dc29917ffbb99fef67
JozeSIMAO <jozesimao10@gmail.com>
2023-11-13 22:06:46 +0200

    8-square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const sizeSquare = process.argv[2];
+if (typeof sizeSquare === 'undefined' || isNaN(sizeSquare)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < sizeSquare; i++) {
+    let row = '';
+    for (let j = 0; j < sizeSquare; j++) {
+      row += 'X ';
+    }
+    console.log(row);
+  }
+}

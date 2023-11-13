1c175b2da7af3e975f3ef661fcbde4312dffa280
JozeSIMAO <jozesimao10@gmail.com>
2023-11-13 15:43:16 +0200

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,9 @@ arguments = process.argv;
 
 if (arguments.length === 2) {
   console.log('No argument');
+}
+else if (arguments.length === 3) {
+    console.log('Argument found');
 } else {
   console.log('Arguments found');
 }

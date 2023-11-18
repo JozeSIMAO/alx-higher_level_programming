e778bdaab9cbdd6739367f32d88cb6c6358516c9
Joze <joze@kali.joze.org>
2023-11-18 22:48:25 +0200

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes an argument and displays all values in the states table
+of htbn_0e_0_usa where name matches the argument"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    match = sys.argv[4]
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (match))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()

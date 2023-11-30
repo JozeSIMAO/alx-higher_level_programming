bde321ac43536567d24d52dfc27daba5be8de6ff
JozeSIMAO <jozesimao10@gmail.com>
2023-11-30 17:35:03 +0200

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+Finds a peak in a list of unsorted integers.
+"""
+
+
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers"""
-    
+    """Finds a peak in the list."""
     if not list_of_integers:
         return None
 
@@ -10,16 +14,11 @@ def find_peak(list_of_integers):
     while low <= high:
         mid = (low + high) // 2
 
-        # Check if mid is a peak
-        if (mid == 0 or list_of_integers[mid - 1]
-            <= list_of_integers[mid]) and \
-           (mid == len(list_of_integers) - 1 or list_of_integers[mid + 1]
-                <= list_of_integers[mid]):
+        if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and \
+           (mid == len(list_of_integers) - 1 or list_of_integers[mid + 1] <= list_of_integers[mid]):
             return list_of_integers[mid]
 
-        # If the element to the left is greater, peak must be on the left side
         if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
             high = mid - 1
         else:
-            # Peak must be on the right side
             low = mid + 1

e633424e7b30d6a06de1f5f0fff87dd10dfd6c69
JozeSIMAO <jozesimao10@gmail.com>
2023-12-01 13:51:24 +0200

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email, sends a POST request, and displays the body of the response
+Takes in a URL and an email, sends a POST request
+and displays the body of the response
 """
 
 import requests

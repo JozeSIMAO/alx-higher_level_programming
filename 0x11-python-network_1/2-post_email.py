86af246477c82e3b806ce7661f15d500ec03aed9
JozeSIMAO <jozesimao10@gmail.com>
2023-12-01 13:53:18 +0200

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email, sends a POST request
-and displays the body of the response
+Takes in a URL and an email, sends a POST request, and displays the body of the response
 """
 
-import requests
+import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
-
-    try:
-        response = requests.post(url, data=data)
-        response.raise_for_status()
-
-        content = response.text
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data=data) as response:
+        content = response.read().decode('utf-8')
         print(f"Your email is: {content}")
-    except requests.RequestException as e:
-        print(f"Error: {e}")

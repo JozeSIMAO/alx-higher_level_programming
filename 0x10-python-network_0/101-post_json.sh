b190c8f26396a97e7ffa17bf5c9fbb9863294ceb
JozeSIMAO <jozesimao10@gmail.com>
2023-12-01 14:36:28 +0200

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with the contents of a file
-if [ -f "$2" ]; then
-    curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" | grep "Valid JSON" && echo "" || echo "Not a valid JSON"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" | grep "Valid JSON" && echo "" || echo "Not a valid JSON"

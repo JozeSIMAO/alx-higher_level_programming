ec1fe4ac3c758920f64a11ba23c9ccf1077f24a5
JozeSIMAO <jozesimao10@gmail.com>
2023-12-01 14:02:56 +0200

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request, and displays the value of the variable X-Request-Id
+Uses requests package
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    header_value = response.headers.get('X-Request-Id')
+    print(header_value)
